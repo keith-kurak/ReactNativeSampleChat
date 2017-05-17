ea9d8f0bdd5e56f41985169df6accf95c4f8fe4b
Keith Kurak <keith.kurak@gmail.com>
2017-05-17 00:34:45 -0400

    ...

--- app/routes/settings/SettingsView.js
+++ app/routes/settings/SettingsView.js
@@ -8,8 +8,8 @@ export default class SettingsView extends Component {
   render() {
     return (
       <View>
-        <Text>Settings view</Text>
         <Text>It should say the name of the platform below, since this is demonstrating platform-specific code</Text>
+        <Text>You'll also see a button that is yellow because of a global style that happens to be set to the color yellow</Text>
         <Text/>
         <Button text="Some button"/>
       </View>
